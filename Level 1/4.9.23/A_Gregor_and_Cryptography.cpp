36b0cf9b1cdf270fef92a79bde6113530d4bc214
Rajarshi Roy <roystark.dev@gmail.com>
2023-09-05 20:44:02 +0530

    Sorted files

---
+++ Level 1/4.9.23/A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define debug(x) cout << #x << ' ' << x << endl;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        ll n; cin >> n;
+        cout << 2 << ' ' << n - 1 << endl;
+    }
+}
